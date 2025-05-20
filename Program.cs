6520270f65c9b2ebaebe8dbcb780436c6780c8d6
joaquinlechesi <joaquinlechesi@gmail.com>
2025-05-20 12:27:08 -0300

    CalculadoraV1

--- Program.cs
+++ Program.cs
@@ -1,14 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 
-int a;
+// int a;
 
-int b;
+// int b;
 
-a = 10;
+// a = 10;
 
-b = a;
+// b = a;
 
-Console.WriteLine("valor de a:"+a);
+// Console.WriteLine("valor de a:"+a);
 
-Console.WriteLine("valor de b:"+b);
+// Console.WriteLine("valor de b:" + b);
+
+// Console.WriteLine("INVERTIR NUMERO");
+
+// string cadenaNumero = Console.ReadLine();
+// int numero;
+// int.TryParse(cadenaNumero, out numero);
+// int aux = 0;
+
+// if (numero > 0)
+// {
+//     while (numero > 10)
+//     {
+//         aux = aux * 10 + numero % 10;
+//         numero = numero / 10;
+//     }
+//     aux = aux * 10 + numero;
+// }else
+// {
+//     aux = numero;
+// }
+// Console.WriteLine(aux);
+
+Console.WriteLine("CalculadoraV1");
