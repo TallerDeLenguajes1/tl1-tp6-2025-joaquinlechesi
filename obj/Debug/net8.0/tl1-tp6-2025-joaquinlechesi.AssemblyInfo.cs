fe599853c36e11aa58ccfb9b184af5f50046ff1c
joaquinlechesi <joaquinlechesi@gmail.com>
2025-06-01 09:25:33 -0300

    archivos nuevos

--- obj/Debug/net8.0/tl1-tp6-2025-joaquinlechesi.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl1-tp6-2025-joaquinlechesi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl1-tp6-2025-joaquinlechesi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75f54b6e2a2089f397e3df844ae335de34447e1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d66dd4ac8392dd001d5214e38b2abb57f637db49")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl1-tp6-2025-joaquinlechesi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl1-tp6-2025-joaquinlechesi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
