a4df22afd16e5c81081c126cc5dac1ae6c534c27
joaquinlechesi <joaquinlechesi@gmail.com>
2025-06-02 01:40:56 -0300

    Extraer una subcadena

--- Program.cs
+++ Program.cs
@@ -16,3 +16,23 @@ cadenaFinal = cadena + cadena2;
 Console.WriteLine("La cadena final es: " + cadenaFinal);
 
 // Extraer una subcadena de la cadena ingresada
+
+// Trabajar con SubString()
+
+Console.WriteLine("Ingrese una nueva cadena:");
+cadena = Console.ReadLine();
+int subcadena;
+Console.WriteLine("Ingrese un numero para obtener una subcadena:");
+int.TryParse(Console.ReadLine(), out subcadena);
+//cadena2 = Console.ReadLine();
+if (subcadena <= cadena.Length)
+{
+    Console.WriteLine("La subcadena obtenida es: " + cadena.Substring(subcadena));
+}
+else
+{
+    Console.WriteLine("El numero ingresado mayor a la longitud de la cadena.");
+}
+
+// Utilizando la calculadora creada anteriormente realizar las operaciones de dos números y mostrar por pantalla y mostrar en texto el resultado. Por ejemplo para la suma sería: “la suma de “ num1 “ y de” num2 “ es igual a: ” resultado.
+
